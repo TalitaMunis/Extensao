9f120341a000e79566df3cc4ca2df6ba43d7ab0b
Pedro Vieira <pedroalbuquerque4455@gmail.com>
2025-08-30 00:03:42 -0300

    criando pastas, properties

---
+++ src/main/java/com/extensao/saude_estilo_vida/SaudeEstiloVidaApplication.java
@@ -0,0 +1,13 @@
+package com.extensao.saude_estilo_vida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeEstiloVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeEstiloVidaApplication.class, args);
+	}
+
+}
